caab8752d0b37592fc07259b982908c08735bdc7
Andrey Mirt <nrmously@gmail.com>
2024-08-30 15:30:17 +0300

    Создал отдельную прилу для работы с юзерами, там же описаны все работы с токеном, работа с почтой, система регистрации и аутентификации и права доступа.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,32 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from users.views import UserCreateViewSet, UserReceiveTokenViewSet, UserViewSet
+
+app_name = 'api'
 
 router_v1 = DefaultRouter()
 
+router_v1.register("users", UserViewSet, basename="users")
 router_v1.register('titles', TitleViewSet, basename='Title')
 router_v1.register('categories', CategoryViewSet)
 router_v1.register('genres', GenreViewSet)
 
+auth_urls_v1 = [
+    path(
+        "signup/",
+        UserCreateViewSet.as_view({"post": "create"}),
+        name="signup"
+    ),
+    path(
+        "token/",
+        UserReceiveTokenViewSet.as_view({"post": "create"}),
+        name="token"
+    ),
+]
+
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path("auth/", include(auth_urls_v1)),
 ]
