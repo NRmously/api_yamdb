f5cd88aa0ce82a0ca66d107ab028504a44e15f1a
medmikhr <dmitry.khristenko@gmail.com>
2024-09-03 22:55:37 +0300

    изменения после permissions. остались две ошибки

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,13 +1,30 @@
 import datetime as dt
 from rest_framework import serializers
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, GenreTitle
 
 
-class TitleSerializer(serializers.ModelSerializer):
+class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = '__all__'
+        model = Genre
+        fields = ('name', 'slug')
+        lookup_field = 'slug'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+        lookup_field = 'slug'
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='slug', queryset=Category.objects.all())
+    genre = serializers.SlugRelatedField(many=True, slug_field='slug', queryset=Genre.objects.all())
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'category', 'genre', 'description')
         model = Title
 
     def validate_year(self, value):
@@ -16,15 +33,10 @@ class TitleSerializer(serializers.ModelSerializer):
         return value
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class TitleGetSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(many=True, read_only=True)
 
     class Meta:
-        fields = '__all__'
-        model = Category
-
-
-class GenreSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = '__all__'
-        model = Genre
+        fields = ('id', 'name', 'year', 'category', 'genre', 'description')
+        model = Title
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework import filters, viewsets
 
 from reviews.models import Category, Genre, Title
-from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer, TitleGetSerializer
 from .permissions import IsAdminOrReadOnly
 
 
@@ -12,25 +11,32 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     permission_classes = (IsAdminOrReadOnly,)
-    filter_backends = (DjangoFilterBackend,)
-    filterset_fields = '__all__'
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
+    filterset_fields = ('name', 'year', 'category', 'genre', 'category__slug', 'genre__slug',)
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return TitleGetSerializer
+        return TitleSerializer
 
 
-class CategoryViewSet(viewsets.GenericViewSet,
-                      mixins.CreateModelMixin,
-                      mixins.ListModelMixin):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = 'slug'
     permission_classes = (IsAdminOrReadOnly,)
+    # pagination_class = PageNumberPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
 
 
-class GenreViewSet(viewsets.GenericViewSet,
-                      mixins.CreateModelMixin,
-                      mixins.ListModelMixin):
+class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    lookup_field = 'slug'
     permission_classes = (IsAdminOrReadOnly,)
+    # pagination_class = LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
+
