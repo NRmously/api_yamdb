83d29348bd6c3a69eaea142ddd2cc7ea417a9efe
Losev <qwinstonsket4er@gmail.com>
2024-09-07 12:39:48 +0300

    Тесты прошли

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -71,7 +71,7 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments'
     )
-    text = models.TextField
+    text = models.TextField()
     review = models.ForeignKey(
         Review, on_delete=models.CASCADE, related_name='comments'
     )
